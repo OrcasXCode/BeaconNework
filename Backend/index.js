271e8bae3209b5b2f0ee41da202a02eb6b4d8e1e
Om-Sureja <omsureja@gmail.com>
2024-02-26 22:51:30 +0530

    punny bug

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,20 @@
 const express = require("express");
-const app=express();
-const bodyParser=require("body-parser")
-const userRouter=require("../Backend/routes/User")
-const cors=require("cors")
-const dotenv=require("dotenv")
-const database=require("./config/database")
-
-
+const app = express();
+const bodyParser = require("body-parser");
+const userRouter = require("../Backend/routes/User");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const database = require("./config/database");
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(bodyParser.json());
 
-
 dotenv.config();
-database.connect()
+database.connect();
 
-app.use("/user",userRouter)
+app.use("/user", userRouter);
 
-
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
-})
-    
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
