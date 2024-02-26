36b46d7d4a100539b7b8bb6d6ec4f3dc466a0ca3
Om-Sureja <omsureja@gmail.com>
2024-02-26 22:59:43 +0530

    bug1

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,19 +1,19 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-
-const {MONGODB_URL}=process.env;
+const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
-	mongoose
-		.connect(MONGODB_URL, {
-			// useNewUrlparser: true,
-			// useUnifiedTopology: true,
-		})
-		.then(console.log(`DB Connection Success`))
-		.catch((err) => {
-			console.log(`DB Connection Failed`);
-			console.log(err);
-			process.exit(1);
-		});
+  //   console.log("MongoDB URL:", MONGODB_URL); // Add this line for debugging
+  mongoose
+    .connect(MONGODB_URL, {
+      // useNewUrlparser: true,
+      // useUnifiedTopology: true,
+    })
+    .then(console.log(`DB Connection Success`))
+    .catch((err) => {
+      console.log(`DB Connection Failed`);
+      console.log(err);
+      process.exit(1);
+    });
 };
