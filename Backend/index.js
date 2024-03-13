306412aa1a11a602939f1dbafd41b37a2553bc95
Om-Sureja <omsureja@gmail.com>
2024-03-14 00:34:11 +0530

    change

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,16 @@ const userRouter = require("../Backend/routes/User");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const database = require("./config/database");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-
+app.use(express.static("public"));
+app.get("/*", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+  res.sendFile(filePath);
+});
 dotenv.config();
 database.connect();
 
