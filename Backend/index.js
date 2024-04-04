a5d8ef284e6eb0ec1eacc6b609b5b2c1523e838c
Om-Sureja <omsureja@gmail.com>
2024-04-04 23:51:51 +0530

    c1

--- Backend/index.js
+++ Backend/index.js
@@ -6,50 +6,11 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const database = require("./config/database");
 const path = require("path");
-const session = require("express-session");
-const passport = require("./auth"); // Import Passport middleware
-const { isLoggedIn } = require("./auth"); // Import isLoggedIn middleware
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.static("public"));
-
-// Configure session middleware
-// app.use(
-//   session({
-//     name: "token",
-//     secret: "cats",
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
-
-// // Initialize Passport middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Authentication routes
-// app.get("/", (req, res) => {
-//   res.send('<a href="/auth/google">Authenticate with Google</a>');
-// });
-
-// app.get(
-//   "/auth/google",
-//   passport.authenticate("google", { scope: ["email", "profile"] })
-// );
-
-// app.get(
-//   "/auth/google/callback",
-//   passport.authenticate("google", {
-//     successRedirect: "/",
-//     failureRedirect: "/auth/google/failure",
-//   })
-// );
-
-// app.get("/auth/google/failure", (req, res) => {
-//   res.send("Failed to authenticate..");
-// });
 
 // Serve static files
 app.get("/*", (req, res) => {
